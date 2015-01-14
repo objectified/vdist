bd536d07da42b582fb4db09cda300a8100a1e371
objectified <objectified@gmail.com>
2015-01-14 10:13:22 +0100

    add scipycentral example

---
+++ examples/assemble_scipycentral.py
@@ -0,0 +1,13 @@
+from vdist.builder import Builder
+
+builder = Builder()
+
+builder.add_build(
+    name='SciPyCentral builder :: centos6',
+    app='SciPyCentral',
+    version='1.0',
+    git_url='https://github.com/scipy/SciPyCentral',
+    build_machine='centos6'
+)
+
+builder.build()
