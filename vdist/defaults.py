726734676412fd4a39c7253d3f3e7333d86d4d06
objectified <objectified@gmail.com>
2015-01-28 08:10:17 +0100

    externalize default locations, rename templates

---
+++ vdist/defaults.py
@@ -0,0 +1,7 @@
+PYTHON_BASEDIR='/opt/vdist-python'
+PYTHON_VERSION='2.7.9'
+LOCAL_PROFILES_DIR='buildprofiles'
+LOCAL_PROFILES_FILE='profiles.json'
+BUILD_BASEDIR='dist'
+SCRATCH_BUILDSCRIPT_NAME='buildscript.sh'
+SCRATCH_DIR='scratch'
