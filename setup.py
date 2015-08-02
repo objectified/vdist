1d3f3ad54ee65e4eda875528798bb1e1577342d6
objectified <objectified@gmail.com>
2015-08-02 14:10:20 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vdist',
-    version='0.3.7',
+    version='0.4',
     description='Create OS packages from Python projects using Docker containers',
     long_description='Create OS packages from Python projects using Docker containers',
     author='L. Brouwer',
