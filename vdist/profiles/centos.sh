05f219b64bae08f73c34d46011ce2fc9167e6c07
dante-signal31 <dante.signal31@gmail.com>
2016-01-05 16:36:05 +0100

    Centos template updated with packaging_type parameter.

--- vdist/profiles/centos.sh
+++ vdist/profiles/centos.sh
@@ -109,10 +109,18 @@ cd /
 find {{package_build_root}} -type d -name '.git' -print0 | xargs -0 rm -rf
 find {{package_build_root}} -type d -name '.svn' -print0 | xargs -0 rm -rf
 
-{% if custom_filename %}
-    fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}}/{{custom_filename}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {{package_build_root}}/{{project_root}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
-{% else %}
-    fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {{package_build_root}}/{{project_root}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
+{% if packaging_type == "dir" %}
+    {% if custom_filename %}
+        fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}}/{{custom_filename}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {{package_build_root}}/{{project_root}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
+    {% else %}
+        fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {{package_build_root}}/{{project_root}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
+    {% endif %}
+{% elif packaging_type == "setup" %}
+    {% if custom_filename %}
+        fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}}/{{custom_filename}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
+    {% else %}
+        fpm -s {{source_type}} -t rpm -n {{app}} -p {{package_build_root}} -v {{version}} {% for dep in runtime_deps %} --depends {{dep}} {% endfor %} {{fpm_args}} {% if compile_python %} $PYTHON_BASEDIR {% endif %}
+    {% endif %}
 {% endif %}
 
 cp {{package_build_root}}/*rpm {{shared_dir}}
