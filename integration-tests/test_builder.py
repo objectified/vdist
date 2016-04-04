33873f0be3eee71f3f657eae7735472c13b389f4
dante-signal31 <dante.signal31@gmail.com>
2016-04-04 17:40:16 +0200

    Test for scenario 2 pass in centos.

--- integration-tests/test_builder.py
+++ integration-tests/test_builder.py
@@ -156,39 +156,39 @@ def _get_purged_deb_file_list(deb_filepath, file_filter):
 #     assert geolocate_launcher in file_list_purged
 
 
-def test_generate_rpm_from_get_setup_compile():
-    builder_parameters = {
-        "app": 'geolocate',
-        "version": '1.3.0',
-        "source": git(
-            uri='https://github.com/dante-signal31/geolocate',
-            branch='master'
-        ),
-        "profile": 'centos7',
-        "compile_python": True,
-        "python_version": '3.4.3',
-        "fpm_args": '--maintainer dante.signal31@gmail.com -a native --url '
-                    'https://github.com/dante-signal31/geolocate --description '
-                    '"This program accepts any text and searchs inside every IP'
-                    ' address. With each of those IP addresses, '
-                    'geolocate queries '
-                    'Maxmind GeoIP database to look for the city and '
-                    'country where'
-                    ' IP address or URL is located. Geolocate is designed to be'
-                    ' used in console with pipes and redirections along with '
-                    'applications like traceroute, nslookup, etc.'
-                    ' " --license BSD-3 --category net',
-        "requirements_path": '/REQUIREMENTS.txt'
-    }
-    target_file = _generate_rpm(builder_parameters)
-    file_list = _read_rpm_contents(target_file)
-    # At this point only a folder should remain if everything is correct.
-    correct_install_path = "/opt/geolocate"
-    assert all((True if correct_install_path in file_entry else False
-                for file_entry in file_list))
-    # Geolocate launcher should be in bin folder too.
-    geolocate_launcher = "/opt/geolocate/bin/geolocate"
-    assert geolocate_launcher in file_list
+# def test_generate_rpm_from_get_setup_compile():
+#     builder_parameters = {
+#         "app": 'geolocate',
+#         "version": '1.3.0',
+#         "source": git(
+#             uri='https://github.com/dante-signal31/geolocate',
+#             branch='master'
+#         ),
+#         "profile": 'centos7',
+#         "compile_python": True,
+#         "python_version": '3.4.3',
+#         "fpm_args": '--maintainer dante.signal31@gmail.com -a native --url '
+#                     'https://github.com/dante-signal31/geolocate --description '
+#                     '"This program accepts any text and searchs inside every IP'
+#                     ' address. With each of those IP addresses, '
+#                     'geolocate queries '
+#                     'Maxmind GeoIP database to look for the city and '
+#                     'country where'
+#                     ' IP address or URL is located. Geolocate is designed to be'
+#                     ' used in console with pipes and redirections along with '
+#                     'applications like traceroute, nslookup, etc.'
+#                     ' " --license BSD-3 --category net',
+#         "requirements_path": '/REQUIREMENTS.txt'
+#     }
+#     target_file = _generate_rpm(builder_parameters)
+#     file_list = _read_rpm_contents(target_file)
+#     # At this point only a folder should remain if everything is correct.
+#     correct_install_path = "/opt/geolocate"
+#     assert all((True if correct_install_path in file_entry else False
+#                 for file_entry in file_list))
+#     # Geolocate launcher should be in bin folder too.
+#     geolocate_launcher = "/opt/geolocate/bin/geolocate"
+#     assert geolocate_launcher in file_list
 
 #
 # # Scenario 2.- Project not containing a setup.py and compiles Python -> package
@@ -217,7 +217,32 @@ def test_generate_rpm_from_get_setup_compile():
 #                for file_entry in file_list_purged)
 #     assert any(correct_folders[1] in file_entry
 #                for file_entry in file_list_purged)
-#
+
+
+def test_generate_rpm_from_git_nosetup_compile():
+    builder_parameters = {"app": 'jtrouble',
+                          "version": '1.0.0',
+                          "source": git(
+                                uri='https://github.com/objectified/jtrouble',
+                                branch='master'
+                          ),
+                          "profile": 'centos7',
+                          "package_install_root": "/opt",
+                          "python_basedir": "/opt/python",
+                          "compile_python": True,
+                          "python_version": '3.4.3', }
+    target_file = _generate_rpm(builder_parameters)
+    file_list = _read_rpm_contents(target_file)
+    # At this point only two folders should remain if everything is correct:
+    # application folder and compiled interpreter folder.
+    correct_folders = ["/opt/jtrouble", "/opt/python"]
+    assert all((True if any(folder in file_entry for folder in correct_folders)
+                else False
+                for file_entry in file_list))
+    assert any(correct_folders[0] in file_entry
+               for file_entry in file_list)
+    assert any(correct_folders[1] in file_entry
+               for file_entry in file_list)
 #
 # # Scenario 3 - Project containing a setup.py and using a prebuilt Python package
 # # (e.g. not compiling) -> package the custom Python basedir only.
