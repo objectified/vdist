b398a2e83033c8bc3e4d670a9cb0d668b477a2c4
dante-signal31 <dante.signal31@gmail.com>
2016-04-05 16:36:20 +0200

    Test for scenario 4 pass in centos.

--- integration-tests/test_builder.py
+++ integration-tests/test_builder.py
@@ -292,50 +292,50 @@ def _get_purged_deb_file_list(deb_filepath, file_filter):
 #     assert geolocate_launcher in file_list_purged
 
 
-def test_generate_rpm_from_git_setup_nocompile():
-    builder_parameters = {
-        "app": 'geolocate',
-        "version": '1.3.0',
-        "source": git(
-            uri='https://github.com/dante-signal31/geolocate',
-            branch='master'
-        ),
-        "profile": 'centos7',
-        "compile_python": False,
-        "python_version": '3.4.3',
-        # Lets suppose custom python package is already installed and its root
-        # folder is /usr. Actually I'm using default installed python3
-        # package, it's is going to be a huge package but this way don't
-        # need a private package repository.
-        "python_basedir": '/usr',
-        "fpm_args": '--maintainer dante.signal31@gmail.com -a native --url '
-                    'https://github.com/dante-signal31/geolocate --description '
-                    '"This program accepts any text and searchs inside'
-                    ' every IP '
-                    'address. With each of those IP addresses, '
-                    'geolocate queries '
-                    'Maxmind GeoIP database to look for the city and '
-                    'country where'
-                    ' IP address or URL is located. Geolocate is designed to be'
-                    ' used in console with pipes and redirections along with '
-                    'applications like traceroute, nslookup, etc.'
-                    ' " --license BSD-3 --category net',
-        "requirements_path": '/REQUIREMENTS.txt'
-    }
-    target_file = _generate_rpm(builder_parameters)
-    file_list = _read_rpm_contents(target_file)
-    # At this point only a folder should remain if everything is correct.
-    correct_install_path = "/usr"
-    assert all((True if correct_install_path in file_entry else False
-                for file_entry in file_list))
-    # If python basedir was properly packaged then /usr/bin/python should be
-    # there.
-    python_interpreter = "/usr/bin/python2.7"
-    assert python_interpreter in file_list
-    # If application was properly packaged then launcher should be in bin folder
-    # too.
-    geolocate_launcher = "/usr/bin/geolocate"
-    assert geolocate_launcher in file_list
+# def test_generate_rpm_from_git_setup_nocompile():
+#     builder_parameters = {
+#         "app": 'geolocate',
+#         "version": '1.3.0',
+#         "source": git(
+#             uri='https://github.com/dante-signal31/geolocate',
+#             branch='master'
+#         ),
+#         "profile": 'centos7',
+#         "compile_python": False,
+#         "python_version": '3.4.3',
+#         # Lets suppose custom python package is already installed and its root
+#         # folder is /usr. Actually I'm using default installed python3
+#         # package, it's is going to be a huge package but this way don't
+#         # need a private package repository.
+#         "python_basedir": '/usr',
+#         "fpm_args": '--maintainer dante.signal31@gmail.com -a native --url '
+#                     'https://github.com/dante-signal31/geolocate --description '
+#                     '"This program accepts any text and searchs inside'
+#                     ' every IP '
+#                     'address. With each of those IP addresses, '
+#                     'geolocate queries '
+#                     'Maxmind GeoIP database to look for the city and '
+#                     'country where'
+#                     ' IP address or URL is located. Geolocate is designed to be'
+#                     ' used in console with pipes and redirections along with '
+#                     'applications like traceroute, nslookup, etc.'
+#                     ' " --license BSD-3 --category net',
+#         "requirements_path": '/REQUIREMENTS.txt'
+#     }
+#     target_file = _generate_rpm(builder_parameters)
+#     file_list = _read_rpm_contents(target_file)
+#     # At this point only a folder should remain if everything is correct.
+#     correct_install_path = "/usr"
+#     assert all((True if correct_install_path in file_entry else False
+#                 for file_entry in file_list))
+#     # If python basedir was properly packaged then /usr/bin/python should be
+#     # there.
+#     python_interpreter = "/usr/bin/python2.7"
+#     assert python_interpreter in file_list
+#     # If application was properly packaged then launcher should be in bin folder
+#     # too.
+#     geolocate_launcher = "/usr/bin/geolocate"
+#     assert geolocate_launcher in file_list
 #
 # # Scenario 4.- Project not containing a setup.py and using a prebuilt Python
 # # package -> package both the project dir and the Python basedir
@@ -366,7 +366,34 @@ def test_generate_rpm_from_git_setup_nocompile():
 #     # there.
 #     python_interpreter = "./usr/bin/python2.7"
 #     assert python_interpreter in file_list_purged
-#
+
+def test_generate_rpm_from_git_nosetup_nocompile():
+    builder_parameters = {
+        "app": 'jtrouble',
+        "version": '1.0.0',
+        "source": git(
+            uri='https://github.com/objectified/jtrouble',
+            branch='master'
+        ),
+        "profile": 'centos7',
+        "compile_python": False,
+        # Here happens the same than in
+        # test_generate_deb_from_git_setup_nocompile()
+        "python_version": '3.4.3',
+        "python_basedir": '/usr',
+    }
+    target_file = _generate_rpm(builder_parameters)
+    file_list = _read_rpm_contents(target_file)
+    # At this point only two folders should remain if everything is correct:
+    # application folder and python basedir folder.
+    correct_folders = ["/opt/jtrouble", "/usr"]
+    assert all((True if any(folder in file_entry for folder in correct_folders)
+                else False
+                for file_entry in file_list))
+    # If python basedir was properly packaged then /usr/bin/python should be
+    # there.
+    python_interpreter = "/usr/bin/python2.7"
+    assert python_interpreter in file_list
 #
 # def test_generate_deb_from_git_suffixed():
 #     builder_parameters = {"app": 'vdist-test-generate-deb-from-git-suffixed',
@@ -377,7 +404,18 @@ def test_generate_rpm_from_git_setup_nocompile():
 #                           ),
 #                           "profile": 'ubuntu-trusty'}
 #     _ = _generate_deb(builder_parameters)
-#
+
+
+# def test_generate_rpm_from_git_suffixed():
+#     builder_parameters = {"app": 'vdist-test-generate-deb-from-git-suffixed',
+#                           "version": '1.0',
+#                           "source": git(
+#                             uri='https://github.com/objectified/vdist.git',
+#                             branch='master'
+#                           ),
+#                           "profile": 'centos7'}
+#     _ = _generate_rpm(builder_parameters)
+
 #
 # def test_generate_deb_from_git_directory():
 #     tempdir = tempfile.gettempdir()
@@ -395,8 +433,26 @@ def test_generate_rpm_from_git_setup_nocompile():
 #                                                   branch='master'),
 #                           "profile": 'ubuntu-trusty'}
 #     _ = _generate_deb(builder_parameters)
+
+
+# def test_generate_rpm_from_git_directory():
+#     tempdir = tempfile.gettempdir()
+#     checkout_dir = os.path.join(tempdir, 'vdist')
 #
+#     git_p = subprocess.Popen(
+#         ['git', 'clone',
+#          'https://github.com/objectified/vdist',
+#          checkout_dir])
+#     git_p.communicate()
 #
+#     builder_parameters = {"app": 'vdist-test-generate-deb-from-git-dir',
+#                           "version": '1.0',
+#                           "source": git_directory(path=checkout_dir,
+#                                                   branch='master'),
+#                           "profile": 'centos7'}
+#     _ = _generate_rpm(builder_parameters)
+
+
 # def test_generate_deb_from_directory():
 #     tempdir = tempfile.gettempdir()
 #     checkout_dir = os.path.join(tempdir, 'vdist')
@@ -412,3 +468,20 @@ def test_generate_rpm_from_git_setup_nocompile():
 #                           "source": directory(path=checkout_dir, ),
 #                           "profile": 'ubuntu-trusty'}
 #     _ = _generate_deb(builder_parameters)
+
+
+# def test_generate_rpm_from_directory():
+#     tempdir = tempfile.gettempdir()
+#     checkout_dir = os.path.join(tempdir, 'vdist')
+#
+#     git_p = subprocess.Popen(
+#         ['git', 'clone',
+#          'https://github.com/objectified/vdist',
+#          checkout_dir])
+#     git_p.communicate()
+#
+#     builder_parameters = {"app": 'vdist-test-generate-deb-from-dir',
+#                           "version": '1.0',
+#                           "source": directory(path=checkout_dir, ),
+#                           "profile": 'centos7'}
+#     _ = _generate_rpm(builder_parameters)
