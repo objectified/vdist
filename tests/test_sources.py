5a1eaedb43e456b41803a5b931e64548634c45a8
objectified <objectified@gmail.com>
2016-06-04 15:16:11 -0400

    add tests for all source types

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,4 +1,4 @@
-from vdist.source import git
+from vdist.source import git, directory, git_directory
 
 
 def test_source_type_git():
@@ -7,3 +7,14 @@ def test_source_type_git():
 
     s = git(uri='https://github.com/objectified/vdist.git')
     assert s['uri'] == 'https://github.com/objectified/vdist'
+
+
+def test_source_type_directory():
+    s = directory(path='/foo/bar/baz/')
+    assert s['path'] == '/foo/bar/baz'
+
+
+def test_source_type_git_directory():
+    s = git_directory(path='/foo/bar/', branch='foo')
+    assert s['branch'] == 'foo'
+    assert s['path'] == '/foo/bar'
