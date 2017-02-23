46b2f0e8ce1d102e7c9a82ba5c1a0bc37675e699
objectified <objectified@gmail.com>
2017-02-23 09:53:19 +0100

    bump version before creating a new pypi release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vdist',
-    version='0.5',
+    version='0.6',
     description='Create OS packages from Python '
                 'projects using Docker containers',
     long_description='Create OS packages from Python '
